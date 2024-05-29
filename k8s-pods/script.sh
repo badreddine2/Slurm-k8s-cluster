535c0a4ff0455ff6dec44d86aa1171fa41f8c9db
badreddine2 <badreddine.aji@gmail.com>
2024-05-29 15:20:38 +0200

    playbook vars

--- k8s-pods/script.sh
+++
@@ -1,8 +0,0 @@
-service_info=$(kubectl get svc slurmc-service)
-
-ip_address=$(echo "$service_info" | awk 'NR>1 {print $3}')
-
-echo "IP Address: $ip_address"
-
-sudo sed -i "s/ControlMachine=.*/ControlMachine=$ip_address/" /etc/slurm/slurm.conf
-sudo sed -i "s/ControlAddr=.*/ControlAddr=$ip_address/" /etc/slurm/slurm.conf
